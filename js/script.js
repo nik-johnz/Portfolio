fd2cf581813289e87520006d6750f8898194c14d
nikjohnz <nikhiljohn36@gmail.com>
2024-01-05 04:11:28 +0530

    portfolio

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var Typed = new Typed(".typing",{
+    strings:["","Full Stack Developer",],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
